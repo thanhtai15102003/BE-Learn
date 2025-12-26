4b2c57bfba26587366676708a8d2a44c6cf7434c
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-26 20:48:54 +0700

    Tao Controllers

--- BE-Learn-Product/routes/client/home.route.js
+++ BE-Learn-Product/routes/client/home.route.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('client/pages/home/index');
-});
+const controller = require('../../controllers/client/home.controller');
+router.get('/', controller.index);
 
 module.exports = router;
