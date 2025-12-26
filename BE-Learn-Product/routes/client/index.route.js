6ec2e2894b72d7f99ec85b54283c0be986b2450e
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-26 20:40:34 +0700

    update router

--- BE-Learn-Product/routes/client/index.route.js
+++ BE-Learn-Product/routes/client/index.route.js
@@ -1,10 +1,8 @@
+const homeRoutes = require('./home.route');
+const productRoutes = require('./product.route');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.render('client/pages/home/index');
-    });
+    app.use('/', homeRoutes);
 
-    app.get('/product', (req, res) => {
-        res.render('client/pages/products/index');
-    });
+    app.use('/product', productRoutes);
 }
