34a4b4f0db39c3a247b7c136c8eb8c089b38e5ed
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-28 16:54:16 +0700

    Tinh nang tim kiem ben Client

--- BE-Learn-Product/public/admin/js/script.js
+++ BE-Learn-Product/public/admin/js/script.js
@@ -1,16 +1,34 @@
 const buttonStatus = document.querySelectorAll('[button-status]');
+
 if (buttonStatus.length > 0) {
     let url = new URL(window.location.href);
-    buttonStatus.forEach(button => {
+    buttonStatus.forEach((button) => {
         button.addEventListener('click', () => {
-            const status = button.getAttribute('button-status')
+            const status = button.getAttribute('button-status');
             if (status) {
-                url.searchParams.set('status', status)
-            }
-            else {
-                url.searchParams.delete('status')
+                url.searchParams.set('status', status);
+            } else {
+                url.searchParams.delete('status');
             }
             window.location.href = url.href;
-        })
-    })
+        });
+    });
 }
+
+//Chức năng tìm kiếm sản phẩm
+const formSearch = document.querySelector('#form-search');
+if (formSearch) {
+    let url = new URL(window.location.href);
+    formSearch.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        console.log(keyword);
+        if (keyword) {
+            url.searchParams.set('keyword', keyword);
+        } else {
+            url.searchParams.delete('keyword');
+        }
+        window.location.href = url.href;
+    });
+}
+// End Chức năng tìm kiếm sản phẩm
