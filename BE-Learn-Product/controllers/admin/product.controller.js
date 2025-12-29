9b4f281e2eb3f5d90aa566f38df50a9c37f2619e
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2025-12-29 19:43:57 +0700

    Toi uu tinh nang phan trang

--- BE-Learn-Product/controllers/admin/product.controller.js
+++ BE-Learn-Product/controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require('../../models/product.model');
 const filterStatusHelper = require('../../helpers/filterStatus');
 const searchHelper = require('../../helpers/search');
+const paginationHelper = require('../../helpers/pagination');
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -20,19 +21,16 @@ module.exports.index = async (req, res) => {
     }
 
     //Pagination
-    let objectPagination = {
+    const countProducts = await Product.countDocuments(find);
+    let objectPagination = paginationHelper(
+        {
         limitItem: 4,
         currentPage: 1
-    }
-    if (req.query.page) {
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
-
-    const countProducts = await Product.countDocuments(find);
-    const totalPage = Math.ceil(countProducts / objectPagination.limitItem);
-
-    objectPagination.totalPage = totalPage;
+        },
+        req.query,
+        countProducts
+    );
+    
     //End Pagination
     const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
     res.render('admin/pages/product/index', {
